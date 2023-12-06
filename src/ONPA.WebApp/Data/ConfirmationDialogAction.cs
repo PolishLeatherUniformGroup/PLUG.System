31db4b887c84dd0788ab8b567917af6e047d281f
Tomasz Molis <molitom@chrobinson.com>
2023-12-06 14:36:28 +0100

    toast dialogs and notifications

---
+++ src/ONPA.WebApp/Data/ConfirmationDialogAction.cs
@@ -0,0 +1,34 @@
+﻿namespace ONPA.WebApp.Data
+{
+    public class ConfirmationDialogAction
+    {
+        public string ActionDescription { get; set; }
+        public bool Confirmed { get; set; } = false;
+    }
+
+    public class DecisionDialogAction
+    {
+        public string ActionDescription { get; set; }
+        public DateTime? DecisionDate { get; set; }
+        public string? Decision { get; set; }
+        public bool Confirmed { get; set; } = false;
+    }
+
+    public class AppealDialogAction
+    {
+        public string ActionDescription { get; set; }
+        public DateTime? AppealDate { get; set; }
+        public string? Justification { get; set; }
+        public bool Confirmed { get; set; } = false;
+    }
+
+    public class PaymentDialogAction
+    {
+        public string ActionDescription { get; set; }
+        public DateTime? PaymentDate { get; set; }
+        public decimal? PaidAmount { get; set; }
+        public string? Currency { get; set; }
+        public bool Confirmed { get; set; } = false;
+
+    }
+}
